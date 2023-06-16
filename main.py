447697e568ffc0f35db47015ce51d0880685d88c
Ali <ali.nikan79@gmail.com>
2023-06-16 02:03:53 -0700

    Added jump & gravity

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 score_surface = test_font.render('My game', False, (64, 64, 64))
-score_rectangle = score_surface.get_rect(center = (400, 50))
+score_rectangle = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rectangle = snail_surface.get_rect(bottomright = (600, 300))
+snail_rectangle = snail_surface.get_rect(bottomright=(600, 300))
 
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rectangle = player_surface.get_rect(midbottom = (80, 300))
+player_rectangle = player_surface.get_rect(midbottom=(80, 300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rectangle.collidepoint(event.pos): print('collision')
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rectangle.collidepoint(event.pos) and player_rectangle.bottom >= 30:
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rectangle.bottom >= 30:
+                player_gravity = -20
+
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
@@ -36,14 +43,12 @@ while True:
     snail_rectangle.x -= 4
     if snail_rectangle.right <= 0: snail_rectangle.left = 800
     screen.blit(snail_surface, snail_rectangle)
+
+    # Player
+    player_gravity += 1
+    player_rectangle.y += player_gravity
+    if player_rectangle.bottom >= 300: player_rectangle.bottom = 300
     screen.blit(player_surface, player_rectangle)
-
-    # if player_rectangle.colliderect(snail_rectangle):
-    #     print('collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rectangle.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
